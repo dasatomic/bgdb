032d8fb5e5bd7431b8272ca31adf9451f7ae7f7f
Aleksandar Tomic <AleksandarTomic88@gmail.com>
2020-04-27 20:55:23 +0200

    Initial project setup

---
+++ atomicdbstarter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace atomicdbstarter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
