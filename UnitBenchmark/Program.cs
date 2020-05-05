38839bcca0cf66ce6e5e5f521319f108e5636caf
Aleksandar Tomic <AleksandarTomic88@gmail.com>
2020-05-05 22:27:16 +0200

    Adding benchmark testing

---
+++ UnitBenchmark/Program.cs
@@ -0,0 +1,42 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using MetadataManager;
+using PageManager;
+
+namespace UnitBenchmark
+{
+    [MemoryDiagnoser]
+    public class MetadataBenchmarks
+    {
+        [Benchmark(Baseline = true)]
+        public void CreateTable()
+        {
+            var allocator =
+                new InMemoryPageManager(4096);
+
+            StringHeapCollection stringHeap = new StringHeapCollection(allocator);
+            MetadataManager.MetadataManager mm = new MetadataManager.MetadataManager(allocator, stringHeap, allocator);
+
+            var tm = mm.GetTableManager();
+            const int repCount = 1000;
+
+            for (int i = 1; i < repCount; i++)
+            {
+                tm.CreateObject(new TableCreateDefinition()
+                {
+                    TableName = "T" + i,
+                    ColumnNames = new[] { "a", "b", "c" },
+                    ColumnTypes = new[] { ColumnType.Int, ColumnType.StringPointer, ColumnType.Double }
+                });
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<MetadataBenchmarks>();
+        }
+    }
+}
