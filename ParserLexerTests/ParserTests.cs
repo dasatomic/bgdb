a181419004c748cb5cf707ffee0752136596c435
Aleksandar Tomic <AleksandarTomic88@gmail.com>
2020-05-06 14:23:58 +0200

    Tests for where clause

--- ParserLexerTests/ParserTests.cs
+++ ParserLexerTests/ParserTests.cs
@@ -4,6 +4,7 @@ using FSharp.Text.Parsing;
 using Microsoft.FSharp.Core;
 using System;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 
 namespace ParserLexerTests
 {
@@ -26,6 +27,45 @@ namespace ParserLexerTests
             Sql.sqlStatement statement = SqlParser.start(f, lexbuf);
 
             Assert.AreEqual(new string[] { "x", "y", "z" }, statement.Columns.ToArray());
+        }
+
+        [Test]
+        public void ParsingWhereClause()
+        {
+            string query =
+                @"SELECT x, y, z   
+                FROM t1   
+                WHERE x = 50 AND y = 20";
+
+            var lexbuf = LexBuffer<char>.FromString(query);
+            Func<LexBuffer<char>, SqlParser.token> func = (x) => SqlLexer.tokenize(x);
+            var f = FuncConvert.FromFunc(func);
+            Sql.sqlStatement statement = SqlParser.start(f, lexbuf);
+
+            Assert.AreEqual(new string[] { "x", "y", "z" }, statement.Columns.ToArray());
+            Assert.AreEqual("t1", statement.Table);
+
+            Sql.where whereStatement = statement.Where.Value;
+            Assert.IsTrue(whereStatement.IsAnd);
+
+            Sql.where.And andStatement = (Sql.where.And)whereStatement;
+            Assert.IsTrue(andStatement.Item1.IsCond);
+            Assert.IsTrue(andStatement.Item2.IsCond);
+
+            Sql.where.Cond leftCond = (Sql.where.Cond)andStatement.Item1;
+            Sql.where.Cond rightCond = (Sql.where.Cond)andStatement.Item2;
+
+            Assert.IsTrue(leftCond.Item.Item1.IsString);
+            Assert.IsTrue(((Sql.value.String)leftCond.Item.Item1).Item == "x");
+            Assert.IsTrue(leftCond.Item.Item2.IsEq);
+            Assert.IsTrue(leftCond.Item.Item3.IsInt);
+            Assert.IsTrue(((Sql.value.Int)leftCond.Item.Item3).Item == 50);
+
+            Assert.IsTrue(rightCond.Item.Item1.IsString);
+            Assert.IsTrue(((Sql.value.String)rightCond.Item.Item1).Item == "y");
+            Assert.IsTrue(rightCond.Item.Item2.IsEq);
+            Assert.IsTrue(rightCond.Item.Item3.IsInt);
+            Assert.IsTrue(((Sql.value.Int)rightCond.Item.Item3).Item == 20);
         }
     }
 }
