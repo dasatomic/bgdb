375f85167ed92bba298c6b2076c239c9455b96fe
Aleksandar Tomic <AleksandarTomic88@gmail.com>
2020-07-15 12:15:06 +0200

    Adding more concurrency tests

--- tests/E2EQueryExecutionTests/ConcurrentE2ETests.cs
+++ tests/E2EQueryExecutionTests/ConcurrentE2ETests.cs
@@ -114,5 +114,78 @@ namespace E2EQueryExecutionTests
                 await tran.Commit();
             }
         }
+
+        [Test]
+        public async Task ConcurrentInsertsAndScan()
+        {
+            await using (ITransaction tran = this.logManager.CreateTransaction(pageManager))
+            {
+                string createTableQuery = "CREATE TABLE ConcurrentTable (TYPE_INT a, TYPE_DOUBLE b, TYPE_STRING c)";
+                await this.queryEntryGate.Execute(createTableQuery, tran).ToArrayAsync();
+                await tran.Commit();
+            }
+
+            const int rowCount = 50;
+            const int writerCount = 50;
+            const int readerCount = 10;
+            int totalSum = 0;
+            int totalInsert = 0;
+
+            async Task insertAction()
+            {
+                using (ITransaction tran = this.logManager.CreateTransaction(pageManager, "INSERT_ROWS"))
+                {
+                    for (int i = 1; i <= rowCount; i++)
+                    {
+                        string insertQuery = $"INSERT INTO ConcurrentTable VALUES ({i}, {i + 0.001}, mystring)";
+                        await this.queryEntryGate.Execute(insertQuery, tran).ToArrayAsync();
+                        await tran.Commit();
+                        Interlocked.Add(ref totalSum, i);
+                        Interlocked.Increment(ref totalInsert);
+                        TestContext.Out.WriteLine("Done inserting {0}", i);
+                    }
+                }
+            }
+
+            async Task selectAction()
+            {
+                using (ITransaction tran = this.logManager.CreateTransaction(pageManager, "GET_ROWS"))
+                {
+                    for (int i = 1; i <= rowCount; i++)
+                    {
+                        string selectQuery = @"SELECT a, b, c FROM ConcurrentTable";
+                        Row[] result = await this.queryEntryGate.Execute(selectQuery, tran).ToArrayAsync();
+                        await tran.Commit();
+                    }
+                }
+            }
+
+            List<Task> tasks = new List<Task>();
+            for (int i = 0; i < writerCount; i++)
+            {
+                tasks.Add(Task.Run(insertAction));
+            }
+
+            for (int i = 0; i < readerCount; i++)
+            {
+                tasks.Add(Task.Run(selectAction));
+            }
+
+            await Task.WhenAll(tasks);
+
+            await using (ITransaction tran = this.logManager.CreateTransaction(pageManager, "GET_ROWS"))
+            {
+                string query = @"SELECT a, b, c FROM ConcurrentTable";
+                Row[] result = await this.queryEntryGate.Execute(query, tran).ToArrayAsync();
+
+                Assert.AreEqual(writerCount* rowCount, totalInsert);
+
+                Assert.AreEqual(writerCount * rowCount, result.Length);
+
+                int sum = result.Sum(r => r.IntCols[0]);
+                Assert.AreEqual(totalSum, sum);
+                await tran.Commit();
+            }
+        }
     }
 }
