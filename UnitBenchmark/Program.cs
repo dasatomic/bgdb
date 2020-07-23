de8b0928360fe871a6cfc69439bd9643370522e0
Aleksandar Tomic <AleksandarTomic88@gmail.com>
2020-07-23 13:11:03 +0200

    Minor changes in perf benchmark

--- UnitBenchmark/Program.cs
+++ UnitBenchmark/Program.cs
@@ -1,4 +1,5 @@
 ﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Running;
 using DataStructures;
 using LogManager;
@@ -6,76 +7,87 @@ using MetadataManager;
 using PageManager;
 using QueryProcessing;
 using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 using Test.Common;
 
 namespace UnitBenchmark
 {
-    public class Benchmarks
+    public static class BenchmarkUtils
+    {
+        public static async Task<(ILogManager, IPageManager, QueryEntryGate)> GetLogAndQueryEntryGate()
+        {
+            var pageManager =  new PageManager.PageManager(4096, new FifoEvictionPolicy(100, 5), TestGlobals.DefaultPersistedStream);
+            var logManager = new LogManager.LogManager(new BinaryWriter(new MemoryStream()));
+            StringHeapCollection stringHeap = null;
+
+            await using (ITransaction tran = logManager.CreateTransaction(pageManager, "SETUP"))
+            {
+                stringHeap = new StringHeapCollection(pageManager, tran);
+                await tran.Commit();
+            }
+
+            var metadataManager = new MetadataManager.MetadataManager(pageManager, stringHeap, pageManager, logManager);
+            AstToOpTreeBuilder treeBuilder = new AstToOpTreeBuilder(metadataManager, stringHeap, pageManager);
+
+            var queryEntryGate = new QueryEntryGate(
+                statementHandlers: new ISqlStatement[]
+                {
+                    new CreateTableStatement(metadataManager),
+                    new InsertIntoTableStatement(treeBuilder),
+                    new SelectStatement(treeBuilder),
+                });
+
+            return (logManager, pageManager, queryEntryGate);
+        }
+    }
+
+    public class CreateTableBenchmarks
     {
         [Params(10, 100, 1000)]
         public int TableNumber;
 
-        [Params(10, 100, 1000, 10000)]
+        [Benchmark]
+        public async Task CreateTable()
+        {
+            (ILogManager logManager, IPageManager pageManager, QueryEntryGate queryEntryGate) = await BenchmarkUtils.GetLogAndQueryEntryGate();
+
+            for (int i = 0; i < this.TableNumber; i++)
+            {
+                await using (ITransaction tran = logManager.CreateTransaction(pageManager, "CREATE_TABLE"))
+                {
+                    string createTableQuery = "CREATE TABLE Table (TYPE_INT a, TYPE_DOUBLE b, TYPE_STRING c)";
+                    await queryEntryGate.Execute(createTableQuery, tran).ToArrayAsync();
+                    await tran.Commit();
+                }
+            }
+        }
+    }
+
+    public class InsertTableSingleThreaded
+    {
+        [Params(1000, 10000)]
         public int RowsInTableNumber;
 
         [Benchmark]
-        public void CreateTable()
+        public async Task InsertIntoTable()
         {
-            var allocator =  new PageManager.PageManager(4096, TestGlobals.DefaultEviction, TestGlobals.DefaultPersistedStream);
-            ILogManager logManager = new LogManager.LogManager(new BinaryWriter(new MemoryStream()));
-            ITransaction setupTran = logManager.CreateTransaction(allocator, "SETUP");
-
-            StringHeapCollection stringHeap = new StringHeapCollection(allocator, setupTran);
-            MetadataManager.MetadataManager mm = new MetadataManager.MetadataManager(allocator, stringHeap, allocator, logManager);
-
-            var tm = mm.GetTableManager();
-
-            for (int i = 1; i < TableNumber; i++)
+            (ILogManager logManager, IPageManager pageManager, QueryEntryGate queryEntryGate) = await BenchmarkUtils.GetLogAndQueryEntryGate();
+            await using (ITransaction tran = logManager.CreateTransaction(pageManager, "CREATE_TABLE"))
             {
-                ITransaction tran = logManager.CreateTransaction(allocator, "CT");
-                tm.CreateObject(new TableCreateDefinition()
-                {
-                    TableName = "T" + i,
-                    ColumnNames = new[] { "a", "b", "c" },
-                    ColumnTypes = new[] { ColumnType.Int, ColumnType.StringPointer, ColumnType.Double }
-                }, tran).Wait();
+                string createTableQuery = "CREATE TABLE Table (TYPE_INT a, TYPE_DOUBLE b, TYPE_STRING c)";
+                await queryEntryGate.Execute(createTableQuery, tran).ToArrayAsync();
+                await tran.Commit();
             }
-        }
 
-        [Benchmark]
-        public void InsertIntoTable()
-        {
-            var allocator =  new PageManager.PageManager(4096, TestGlobals.DefaultEviction, TestGlobals.DefaultPersistedStream);
-            ILogManager logManager = new LogManager.LogManager(new BinaryWriter(new MemoryStream()));
-            ITransaction setupTran = logManager.CreateTransaction(allocator, "SETUP");
-
-            StringHeapCollection stringHeap = new StringHeapCollection(allocator, setupTran);
-            MetadataManager.MetadataManager mm = new MetadataManager.MetadataManager(allocator, stringHeap, allocator, logManager);
-            var tm = mm.GetTableManager();
-
-            var columnTypes = new[] { ColumnType.Int, ColumnType.StringPointer, ColumnType.Double };
-            ITransaction tran = logManager.CreateTransaction(allocator, "CT");
-            int id = tm.CreateObject(new TableCreateDefinition()
+            for (int i = 0; i < this.RowsInTableNumber; i++)
             {
-                TableName = "Table",
-                ColumnNames = new[] { "a", "b", "c" },
-                ColumnTypes = columnTypes,
-            }, tran).Result;
-
-            ITransaction tranFT = logManager.CreateTransaction(allocator, "FETCH_TABLE");
-            var table = tm.GetById(id, tranFT).Result;
-            tran.Commit().Wait();
-
-            Row[] source = new Row[] { new Row(new[] { 1 }, new[] { 1.1 }, new[] { "mystring" }, columnTypes) };
-
-            for (int i = 0; i < RowsInTableNumber; i++)
-            {
-                PhyOpStaticRowProvider opStatic = new PhyOpStaticRowProvider(source);
-
-                tran = logManager.CreateTransaction(allocator, "INSERT");
-                PhyOpTableInsert op = new PhyOpTableInsert(table, allocator, stringHeap, opStatic, tran);
-                op.Invoke().Wait();
-                tran.Commit().Wait();
+                await using (ITransaction tran = logManager.CreateTransaction(pageManager, "INSERT"))
+                {
+                    string insertQuery = "INSERT INTO Table VALUES (1, 1.1, mystring)";
+                    await queryEntryGate.Execute(insertQuery, tran).ToArrayAsync();
+                    await tran.Commit();
+                }
             }
         }
     }
@@ -84,7 +96,7 @@ namespace UnitBenchmark
     {
         static void Main(string[] args)
         {
-            var summary = BenchmarkRunner.Run<Benchmarks>();
+            var summary = BenchmarkRunner.Run<InsertTableSingleThreaded>();
         }
     }
 }
