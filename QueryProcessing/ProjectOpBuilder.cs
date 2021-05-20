5e7144afb21ea570ff2cb6002d0f4c030e812af1
dasatomic <AleksandarTomic88@gmail.com>
2021-05-20 14:10:25 +0200

    Prototype for initial function implementation.
    Code is currently pretty ugly and there is a lot of overlap between group by and project ops.
    
    Refactoring is needed.

--- QueryProcessing/ProjectOpBuilder.cs
+++ QueryProcessing/ProjectOpBuilder.cs
@@ -9,8 +9,199 @@ using System.Threading.Tasks;
 
 namespace QueryProcessing
 {
+    class ProjectOrApplyFuncFunctors
+    {
+        /// <summary>
+        /// Functor that projects rowholder into new one.
+        /// Fields that are to be copied are copied.
+        /// Fields on which some execution is to be done
+        /// (e.g. func call) are kept empty and initialized
+        /// with default value.
+        /// </summary>
+        public Func<RowHolder, RowHolder> ProjectAndExtend { get; }
+
+        /// <summary>
+        /// Applies computations on initialized RowHolder
+        /// (i.e. one passed from ProjectAndExtend).
+        /// </summary>
+        public Action<RowHolder> ApplyCompute { get; }
+
+        public MetadataColumn[] ProjectColumnInfo { get; }
+
+        public ProjectOrApplyFuncFunctors(
+            Func<RowHolder, RowHolder> projectAndExtend,
+            Action<RowHolder> applyCompute,
+            MetadataColumn[] projectColumnInfo)
+        {
+            this.ProjectAndExtend = projectAndExtend;
+            this.ApplyCompute = applyCompute;
+            this.ProjectColumnInfo = projectColumnInfo;
+        }
+    }
+
     class ProjectOpBuilder : IStatementTreeBuilder
     {
+        private MetadataColumn[] GetOutputSchema(Sql.columnSelect[] columns, IPhysicalOperator<RowHolder> source)
+        {
+            MetadataColumn[] result = new MetadataColumn[columns.Length];
+            int pos = 0;
+            foreach (Sql.columnSelect column in columns)
+            {
+                if (column.IsProjection)
+                {
+                    var projection = ((Sql.columnSelect.Projection)column);
+                    if (!projection.Item.IsId)
+                    {
+                        throw new Exception("Projection on non id is not supported");
+                    }
+
+                    string projectionId = ((Sql.value.Id)projection.Item).Item;
+                    result[pos] = QueryProcessingAccessors.GetMetadataColumn(projectionId, source.GetOutputColumns());
+                }
+                else if (column.IsFunc)
+                {
+                    var func = ((Sql.columnSelect.Func)column).Item;
+                    Sql.FuncType funcType = func.Item1;
+                    // TODO: Map func type to the right function.
+                    // This should go to new class.
+                    Sql.scalarArgs args = func.Item2;
+
+                    if (funcType.IsAdd)
+                    {
+                        if (!args.IsArgs2)
+                        {
+                            throw new Exception("Sum requires 2 arguments");
+                        }
+
+                        var args2 = ((Sql.scalarArgs.Args2)args).Item;
+                        Sql.value argOne = args2.Item1;
+                        Sql.value argTwo = args2.Item2;
+                        // TODO: Some rules should be applied here to determine output type.
+                        // TODO: For now always return int.
+                        // TODO is keeping 0, 0 here ok?
+                        result[pos] = new MetadataColumn(0, 0, "ADD_Result", new ColumnInfo(ColumnType.Int));
+                    }
+                    else
+                    {
+                        // TODO:
+                        throw new NotImplementedException();
+                    }
+                }
+
+                pos++;
+            }
+
+            return result;
+        }
+
+        private (int?, ColumnInfo?)[] BuildProjectExtendInfo(Sql.columnSelect[] columns, IPhysicalOperator<RowHolder> source)
+        {
+            (int?, ColumnInfo?)[] extendInfo = columns.Select<Sql.columnSelect, (int?, ColumnInfo?)>(c =>
+            {
+                if (c.IsProjection)
+                {
+                    var projection = ((Sql.columnSelect.Projection)c);
+                    if (!projection.Item.IsId)
+                    {
+                        throw new Exception("Projection on non id is not supported");
+                    }
+
+                    string projectionId = ((Sql.value.Id)projection.Item).Item;
+                    MetadataColumn mc = QueryProcessingAccessors.GetMetadataColumn(projectionId, source.GetOutputColumns());
+                    return (mc.ColumnId, null);
+                }
+                else if (c.IsFunc)
+                {
+                    var func = ((Sql.columnSelect.Func)c).Item;
+
+                    Sql.FuncType funcType = func.Item1;
+                    // TODO: Map func type to the right function.
+                    // This should go to new class.
+                    Sql.scalarArgs args = func.Item2;
+
+                    if (funcType.IsAdd)
+                    {
+                        if (!args.IsArgs2)
+                        {
+                            throw new Exception("Sum requires 2 arguments");
+                        }
+
+                        var args2 = ((Sql.scalarArgs.Args2)args).Item;
+                        Sql.value argOne = args2.Item1;
+                        Sql.value argTwo = args2.Item2;
+                        // TODO: Some rules should be applied here to determine output type.
+                        // TODO: For now always return int.
+                        return (null, new ColumnInfo(ColumnType.Int));
+                    }
+                    else
+                    {
+                        // TODO:
+                        throw new NotImplementedException();
+                    }
+                }
+                else
+                {
+                    throw new Exception("Invalid type in select");
+                }
+            }).ToArray();
+
+            return extendInfo;
+        }
+
+        private Action<RowHolder, RowHolder> ExecuteComputeOnRowHolder(IEnumerable<Sql.columnSelect> selects, MetadataColumn[] sourceColumns)
+        {
+            int outputPosition = 0;
+            List<Action<RowHolder /* input */, RowHolder /* output */>> listOfActions = new List<Action<RowHolder, RowHolder>>();
+
+            foreach (var select in selects)
+            {
+                if (!select.IsFunc)
+                {
+                    outputPosition++;
+                    continue;
+                }
+
+                var func = ((Sql.columnSelect.Func)select).Item;
+                Sql.FuncType funcType = func.Item1;
+                // TODO: Map func type to the right function.
+                // This should go to new class.
+                Sql.scalarArgs args = func.Item2;
+
+                if (funcType.IsAdd)
+                {
+                    Sql.scalarArgs.Args2 argsExtracted = (Sql.scalarArgs.Args2)args;
+                    Sql.value arg1 = argsExtracted.Item.Item1;
+                    Sql.value arg2 = argsExtracted.Item.Item2;
+
+                    if (!arg1.IsId || !arg2.IsId)
+                    {
+                        // TODO:
+                        throw new Exception("Only support for ids as function arguments");
+                    }
+
+                    Sql.value.Id arg1Id = (Sql.value.Id)(arg1);
+                    Sql.value.Id arg2Id = (Sql.value.Id)(arg2);
+
+                    MetadataColumn mc1 = QueryProcessingAccessors.GetMetadataColumn(arg1Id.Item, sourceColumns);
+                    MetadataColumn mc2 = QueryProcessingAccessors.GetMetadataColumn(arg2Id.Item, sourceColumns);
+
+                    listOfActions.Add((RowHolder inputRh, RowHolder outputRh) =>
+                    {
+                        QueryProcessingAccessors.ApplyFuncInPlace(
+                            new MetadataColumn[] { mc1, mc2 },
+                            outputPosition,
+                            inputRh,
+                            outputRh,
+                            funcType
+                        );
+                    });
+                }
+            }
+
+            // Execute all the actions.
+            return ((RowHolder input, RowHolder output) => listOfActions.ForEach(act => act(input, output)));
+        }
+
         public Task<IPhysicalOperator<RowHolder>> BuildStatement(Sql.sqlStatement statement, ITransaction tran, IPhysicalOperator<RowHolder> source, InputStringNormalizer inputStringNormalizer)
         {
             Sql.columnSelect[] columns = new Sql.columnSelect[0];
@@ -59,24 +250,14 @@ namespace QueryProcessing
                 // Project Op.
                 List<MetadataColumn> columnMapping = new List<MetadataColumn>();
 
-                foreach (Sql.columnSelect selectColumn in columns)
-                {
-                    if (selectColumn.IsProjection)
-                    {
-                        // TODO: What about func here?
-                        var projection = ((Sql.columnSelect.Projection)selectColumn);
-                        if (!projection.Item.IsId)
-                        {
-                            throw new Exception("Projection on non id is not supported");
-                        }
+                (int?, ColumnInfo?)[] extendInfo = this.BuildProjectExtendInfo(columns, source);
+                Func<RowHolder, RowHolder> projector = (rowHolder) => rowHolder.ProjectAndExtend(extendInfo);
+                Action<RowHolder, RowHolder> computes = ExecuteComputeOnRowHolder(columns, source.GetOutputColumns());
+                MetadataColumn[] outputSchema = this.GetOutputSchema(columns, source);
 
-                        string projectionId = ((Sql.value.Id)projection.Item).Item;
-                        MetadataColumn mc = QueryProcessingAccessors.GetMetadataColumn(projectionId, source.GetOutputColumns());
-                        columnMapping.Add(mc);
-                    }
-                }
+                PhyOpProjectComputeFunctors functors = new PhyOpProjectComputeFunctors(projector, computes);
 
-                IPhysicalOperator<RowHolder> projectOp = new PhyOpProject(source, columnMapping.Select(mc => mc.ColumnId).ToArray(), topRows);
+                IPhysicalOperator<RowHolder> projectOp = new PhyOpProject(source, functors, outputSchema, topRows);
                 return Task.FromResult(projectOp);
             }
         }
---
+++ tests/QueryProcessingTests/PhyOpProjectTests.cs
@@ -0,0 +1,122 @@
+﻿using DataStructures;
+using LogManager;
+using MetadataManager;
+using NUnit.Framework;
+using PageManager;
+using QueryProcessing;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Test.Common;
+
+namespace QueryProcessingTests
+{
+    public class PhyOpProjectTests
+    {
+        private PhyOpScan scan;
+        private ITransaction tran;
+
+        [SetUp]
+        public async Task Setup()
+        {
+            ILogManager logManager;
+            MetadataManager.MetadataManager metadataManager;
+            IPageManager allocator;
+            MetadataTable table;
+            ColumnInfo[] columnInfos;
+
+            allocator =  new PageManager.PageManager(4096, TestGlobals.DefaultEviction, TestGlobals.DefaultPersistedStream);
+            logManager = new LogManager.LogManager(new BinaryWriter(new MemoryStream()));
+            ITransaction setupTran = logManager.CreateTransaction(allocator);
+            StringHeapCollection stringHeap = new StringHeapCollection(allocator, setupTran);
+            metadataManager = new MetadataManager.MetadataManager(allocator, stringHeap, allocator, logManager);
+            var tm = metadataManager.GetTableManager();
+
+            tran = logManager.CreateTransaction(allocator);
+            columnInfos = new[] { new ColumnInfo(ColumnType.Int), new ColumnInfo(ColumnType.String, 1), new ColumnInfo(ColumnType.Double) };
+            int id = await tm.CreateObject(new TableCreateDefinition()
+            {
+                TableName = "Table",
+                ColumnNames = new[] { "a", "b", "c" },
+                ColumnTypes = columnInfos, 
+            }, tran);
+
+            await tran.Commit();
+
+            tran = logManager.CreateTransaction(allocator);
+            table = await tm.GetById(id, tran);
+            await tran.Commit();
+
+            List<RowHolder> source = new List<RowHolder>();
+            for (int i = 0; i < 5; i++)
+            {
+                var rhf = new RowHolder(new[] { new ColumnInfo(ColumnType.Int), new ColumnInfo(ColumnType.String, 1), new ColumnInfo(ColumnType.Double) });
+                rhf.SetField<int>(0, i);
+                rhf.SetField(1, i.ToString().ToCharArray());
+                rhf.SetField<double>(2, i + 0.1);
+                source.Add(rhf);
+            }
+
+            PhyOpStaticRowProvider opStatic = new PhyOpStaticRowProvider(source);
+
+            tran = logManager.CreateTransaction(allocator);
+            PhyOpTableInsert op = new PhyOpTableInsert(table.Collection, opStatic);
+            await op.Iterate(tran).AllResultsAsync();
+            await tran.Commit();
+
+            tran = logManager.CreateTransaction(allocator);
+            PageListCollection pcl = new PageListCollection(allocator, columnInfos, table.RootPage);
+            scan = new PhyOpScan(pcl, tran, table.Columns, "Table");
+        }
+
+        [Test]
+        public async Task ValidateSimpleProject()
+        {
+            PhyOpProjectComputeFunctors functors = new PhyOpProjectComputeFunctors(
+                projector: (rhf) => rhf.Project(new int[] { 0 }),
+                computer: (r1, r2) => { }
+            );
+
+            PhyOpProject project = new PhyOpProject(this.scan, functors, new MetadataColumn[] { this.scan.GetOutputColumns()[0] }, null);
+
+            List<RowHolder> result = new List<RowHolder>();
+            await foreach (var row in project.Iterate(this.tran))
+            {
+                result.Add(row);
+            }
+
+            Assert.AreEqual(5, result.Count);
+            for (int i = 0; i < 5; i++)
+            {
+                Assert.AreEqual(i, result[i].GetField<int>(0));
+            }
+        }
+
+        [Test]
+        public async Task ValidateFuncProject()
+        {
+            PhyOpProjectComputeFunctors functors = new PhyOpProjectComputeFunctors(
+                projector: (rhf) => rhf.Project(new int[] { 0 }),
+                computer: (r1, r2) =>
+                {
+                    int res = r1.GetField<int>(0) + r2.GetField<int>(0);
+                    r2.SetField<int>(0, res);
+                }
+            );
+
+            PhyOpProject project = new PhyOpProject(this.scan, functors, new MetadataColumn[] { this.scan.GetOutputColumns()[0] }, null);
+
+            List<RowHolder> result = new List<RowHolder>();
+            await foreach (var row in project.Iterate(this.tran))
+            {
+                result.Add(row);
+            }
+
+            Assert.AreEqual(5, result.Count);
+            for (int i = 0; i < 5; i++)
+            {
+                Assert.AreEqual(i * 2, result[i].GetField<int>(0));
+            }
+        }
+    }
+}
