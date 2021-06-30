a5407102c1e6023300d86733e1a350dbbb6b1ce0
dasatomic <AleksandarTomic88@gmail.com>
2021-06-30 16:30:34 +0200

    Moving to Xabe.Ffmpeg as ffmpeg wrapper.

--- VideoProcessing/FfmpegVideoChunker.cs
+++ VideoProcessing/FfmpegVideoChunker.cs
@@ -1,15 +1,14 @@
 ﻿using PageManager;
 using System;
-using System.Diagnostics;
 using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
+using Xabe.FFmpeg;
 
 namespace VideoProcessing
 {
     public class FfmpegVideoChunker
     {
-        private const string ffArgsFormat = "-hide_banner -i {0} -c copy -map 0 -segment_time {1} -f segment {2}";
         private readonly string tempDestination;
         private readonly IVideoProcessingInstrumentationInterface logger;
 
@@ -17,38 +16,6 @@ namespace VideoProcessing
         {
             this.tempDestination = tempDestination;
             this.logger = logger;
-        }
-
-        private ProcessStartInfo CreateProcessStartInfo(string ffmpegArgs)
-        {
-            ProcessStartInfo pci = new ProcessStartInfo();
-            if (OperatingSystem.IsWindows())
-            {
-                FileInfo dataRoot = new FileInfo(typeof(FfmpegProbeWrapper).Assembly.Location);
-                string assemblyFolderPath = dataRoot.Directory.FullName;
-                pci.FileName = Path.Combine(assemblyFolderPath, "ffmpeg/ffmpeg.exe");
-                pci.Arguments = ffmpegArgs;
-
-            }
-            else if (OperatingSystem.IsLinux())
-            {
-                // use bash + ffmpeg.
-                // This means that ffmpeg needs to be installed.
-                // TODO: Error handling if ffmpeg isn't installed/bash is not used etc.
-                // For now this is only to pass basic test on Ubuntu when everything is correctly preinstalled.
-                pci.FileName = "/bin/bash";
-                pci.Arguments = $"-c \"ffmpeg {ffmpegArgs}\"";
-            }
-            else
-            {
-                throw new NotImplementedException("Os currently not supported");
-            }
-
-            pci.UseShellExecute = false;
-            pci.RedirectStandardOutput = true;
-            pci.RedirectStandardError = true;
-
-            return pci;
         }
 
         public async Task<string[]> Execute(string fileName, TimeSpan span, ITransaction tran, CancellationToken token)
@@ -61,29 +28,17 @@ namespace VideoProcessing
             tran.RegisterTempFolder(tempDir);
             string outputDestination = Path.Combine(destinationDir, outputFileName);
 
-            string arguments = string.Format(ffArgsFormat, fileName, span, outputDestination);
+            FFmpeg.SetExecutablesPath(OperatingSystem.GetFfmpegExecPath());
+            var conversionResult = await FFmpeg.Conversions.New()
+                .AddParameter($"-i {fileName}")
+                .AddParameter($"-c copy")
+                .AddParameter("-map 0")
+                .AddParameter($"-segment_time {span.TotalSeconds.ToString()}")
+                .AddParameter("-f segment")
+                .SetOutput(outputDestination)
+                .Start();
 
-            ProcessStartInfo pci = CreateProcessStartInfo(arguments);
-            using (Process proc = Process.Start(pci))
-            {
-                this.logger.LogDebug($"Running Process name {proc.ProcessName} with id {proc.Id}.");
-                await proc.WaitForExitAsync(token);
-                this.logger.LogDebug($"Process Id {proc.Id} exited with exit code {proc.ExitCode}.");
-
-                if (!proc.StandardOutput.EndOfStream)
-                {
-                    string output = await proc.StandardOutput.ReadToEndAsync();
-                    output = $"Process id {proc.Id} standard output: " + Environment.NewLine + output;
-                    this.logger.LogDebug(output);
-                }
-
-                if (!proc.StandardError.EndOfStream)
-                {
-                    string output = await proc.StandardError.ReadToEndAsync();
-                    output = $"Process id {proc.Id} standard output: " + Environment.NewLine + output;
-                    this.logger.LogDebug(output);
-                }
-            }
+            this.logger.LogDebug($"Video chunker took {conversionResult.Duration.TotalSeconds}s");
 
             return Directory.GetFiles(destinationDir);
         }
